00cb39a3e357e8e5e5e94730e052a254b398a905
Alessandro Romano <alessandro.romano1992@gmail.com>
2024-05-10 16:27:39 +0000

    updates basic description and favicon stuff

--- src/config.ts
+++ src/config.ts
@@ -24,27 +24,22 @@ export const NAV_ITEMS: NavItems = {
 };
 
 export const SITE = {
-	// Your site's detail?
-	name: "Ink",
-	title: "Astro - Ink",
-	description: "Crisp, minimal, personal blog theme for Astro",
-	url: "https://astro-ink.vercel.app",
-	githubUrl: "https://github.com/one-aalam/astro-ink",
+	name: "Alessandro Romano",
+	title: "Alessandro Romano",
+	description: "The Empathic Tech Leader",
+	url: "https://aleromano.com",
+	githubUrl: "https://github.com/aleromano92",
 	listDrafts: true,
 	image:
 		"https://raw.githubusercontent.com/one-aalam/astro-ink/main/public/astro-banner.png",
 	// YT video channel Id (used in media.astro)
 	ytChannelId: "",
-	// Optional, user/author settings (example)
-	// Author: name
-	author: "", // Example: Fred K. Schott
-	// Author: Twitter handler
-	authorTwitter: "", // Example: FredKSchott
+	author: "Alessandro Romano",
+	authorTwitter: "_aleromano",
 	// Author: Image external source
 	authorImage: "", // Example: https://pbs.twimg.com/profile_images/1272979356529221632/sxvncugt_400x400.jpg, https://abs.twimg.com/sticky/default_profile_images/default_profile_normal.png
-	// Author: Bio
 	authorBio:
-		"Crisp, minimal, personal blog theme for Astro. Crisp, minimal, personal blog theme for Astro. Crisp, minimal, personal blog theme for Astro",
+		"I am addicted to details and obsessed with how things work: from Cloud services to human relationships. Since my first Hello World in RPG Maker, I have come to believe that empathy and emotional intelligence are far more important in the IT field.",
 };
 
 // Ink - Theme configuration
