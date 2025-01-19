146efb1795ec3db3f5db7decaa47261363b8714a
Alessandro Romano <alessandro.romano1992@gmail.com>
2025-01-19 23:07:53 +0100

    amend rss generation after moving to collections

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,17 +1,20 @@
 import rss from '@astrojs/rss';
-
+import { getCollection } from 'astro:content';
+import { BLOG_POSTS_PATH } from '../utils/constants';
 export async function GET(context) {
-    const posts = Object.values(import.meta.glob('./posts/*.md', { eager: true }));
+    const blog = await getCollection('blog');
+    const items = blog.map((post) => ({
+        title: post.data.title,
+        pubDate: post.data.pubDate,
+        description: post.data.description,
+        link: `${BLOG_POSTS_PATH}/${post.slug}/`,
+    }));
+
     return rss({
         title: 'Alessandro Romano\'s Blog',
         description: 'My journey learning and sharing about web development',
         site: context.site,
-        items: posts.map((post) => ({
-            title: post.frontmatter.title,
-            pubDate: post.frontmatter.pubDate,
-            description: post.frontmatter.description,
-            link: post.url,
-        })),
+        items,
         customData: `<language>en-us</language>`,
     });
 }
